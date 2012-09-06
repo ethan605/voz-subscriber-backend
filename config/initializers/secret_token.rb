2af7f25242efe6746f613506600334dfeb6c8326
Ethan N <thanhnx.605@gmail.com>
2012-09-06 09:10:10 +0700

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VozSubscriberBackend::Application.config.secret_token = '058aa433e35caaf92826a25e46e8aa9b584139867af91f215bddbfde5e3d409652019a1088d0c253d912028c161cf0205da9286efa4cc466e031057776db2d29'
